a6514cc80129f2e7bbf74bc8dfee1bfeaa05caf8
B Leung <leungcheukkei@gmail.com>
2020-09-18 02:24:04 +0800

    change index name for deployment, fixed minor bugs

--- javascript/drawingPolygon.js
+++ javascript/drawingPolygon.js
@@ -1,85 +1,88 @@
 class DrawingPolygon extends PaintFunction {
-  constructor(contextReal, contextDraft) {
-    super();
-    this.contextReal = contextReal;
-    this.contextDraft = contextDraft;
-  }
+	constructor(contextReal, contextDraft) {
+		super();
+		this.contextReal = contextReal;
+		this.contextDraft = contextDraft;
+	}
 
-  onMouseDown(coord, event) {
-    this.contextReal.strokeStyle = curStroke;
-    this.contextReal.fillStyle = curFill;
+	onMouseDown(coord, event) {
+		this.contextReal.strokeStyle = curStroke;
+		this.contextReal.fillStyle = curFill;
 
-    this.origX = coord[0];
-    this.origY = coord[1];
-  }
-  onDragging(coord) {
-    let value = document.getElementById("myRange");
-    let input = value.valueAsNumber;
-    this.contextDraft.strokeStyle = curStroke;
-    this.contextDraft.fillStyle = curFill;
-    this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-    var numberOfSides = input;
+		this.origX = coord[0];
+		this.origY = coord[1];
+	}
+	onDragging(coord) {
+		let value = document.getElementById("myRange");
+		let input = value.valueAsNumber;
+		this.contextDraft.strokeStyle = curStroke;
+		this.contextDraft.fillStyle = curFill;
+		this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+		var numberOfSides = input;
 
-    //this is the radius a2+b2=C2//
-    var size = Math.sqrt(
-      Math.pow(this.origX - coord[0], 2) + Math.pow(this.origY - coord[1], 2)
-    );
+		//this is the radius a2+b2=C2//
+		var size = Math.sqrt(
+			Math.pow(this.origX - coord[0], 2) + Math.pow(this.origY - coord[1], 2)
+		);
 
-    var startAngle = coord[1] * 0.005;
-    this.contextDraft.moveTo(
-      this.origX + size * Math.cos(startAngle), // starting X coord.
-      this.origY - size * Math.sin(startAngle) //starting Y coord.
-    );
-    this.contextDraft.beginPath();
-    for (var i = 1; i <= numberOfSides; i += 1) {
-      let rotateAngle = i * ((2 * Math.PI) / numberOfSides);
-      this.contextDraft.lineTo(
-        this.origX +
-          size * Math.cos(startAngle + i * ((2 * Math.PI) / numberOfSides)),
-        this.origY -
-          size * Math.sin(startAngle + i * ((2 * Math.PI) / numberOfSides))
-      );
-    }
-    this.contextDraft.closePath();
-    this.contextDraft.lineWidth = 3;
-    this.contextDraft.stroke();
-    this.contextDraft.fill();
-  }
-  onMouseUp(coord) {
-    let value = document.getElementById("myRange");
-    let input = value.valueAsNumber;
-    this.contextReal.strokeStyle = curStroke;
-    this.contextReal.fillStyle = curFill;
-    this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-    var numberOfSides = input;
+		var startAngle = coord[1] * 0.005;
+		this.contextDraft.moveTo(
+			this.origX + size * Math.cos(startAngle), // starting X coord.
+			this.origY - size * Math.sin(startAngle) //starting Y coord.
+		);
+		this.contextDraft.beginPath();
+		for (var i = 1; i <= numberOfSides; i += 1) {
+			let rotateAngle = i * ((2 * Math.PI) / numberOfSides);
+			this.contextDraft.lineTo(
+				this.origX +
+					size * Math.cos(startAngle + i * ((2 * Math.PI) / numberOfSides)),
+				this.origY -
+					size * Math.sin(startAngle + i * ((2 * Math.PI) / numberOfSides))
+			);
+		}
+		this.contextDraft.closePath();
+		this.contextDraft.lineWidth = curThick;
+		this.contextDraft.stroke();
+		this.contextDraft.fill();
+	}
+	onMouseUp(coord) {
+		let value = document.getElementById("myRange");
+		let input = value.valueAsNumber;
+		this.contextReal.strokeStyle = curStroke;
+		this.contextReal.fillStyle = curFill;
+		this.contextReal.lineWidth = curThick;
 
-    //this is the radius a2+b2=C2//
-    var size = Math.sqrt(
-      Math.pow(this.origX - coord[0], 2) + Math.pow(this.origY - coord[1], 2)
-    );
+		this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+		var numberOfSides = input;
 
-    var startAngle = coord[1] * 0.005;
-    this.contextReal.moveTo(
-      this.origX + size * Math.cos(startAngle), // starting X coord.
-      this.origY - size * Math.sin(startAngle) //starting Y coord.
-    );
-    this.contextReal.beginPath();
-    for (var i = 1; i <= numberOfSides; i += 1) {
-      let rotateAngle = i * ((2 * Math.PI) / numberOfSides);
-      this.contextReal.lineTo(
-        this.origX +
-          size * Math.cos(startAngle + i * ((2 * Math.PI) / numberOfSides)),
-        this.origY -
-          size * Math.sin(startAngle + i * ((2 * Math.PI) / numberOfSides))
-      );
-    }
-    this.contextReal.closePath();
-    this.contextReal.lineWidth = 3;
-    this.contextReal.stroke();
-    this.contextReal.fill();
+		//this is the radius a2+b2=C2//
+		var size = Math.sqrt(
+			Math.pow(this.origX - coord[0], 2) + Math.pow(this.origY - coord[1], 2)
+		);
 
-    this.contextReal.strokeStyle = curStroke;
-    cPush();
-    // console.log(cPushArray);
-  }
+		var startAngle = coord[1] * 0.005;
+		this.contextReal.moveTo(
+			this.origX + size * Math.cos(startAngle), // starting X coord.
+			this.origY - size * Math.sin(startAngle) //starting Y coord.
+		);
+		this.contextReal.beginPath();
+		for (var i = 1; i <= numberOfSides; i += 1) {
+			let rotateAngle = i * ((2 * Math.PI) / numberOfSides);
+			this.contextReal.lineTo(
+				this.origX +
+					size * Math.cos(startAngle + i * ((2 * Math.PI) / numberOfSides)),
+				this.origY -
+					size * Math.sin(startAngle + i * ((2 * Math.PI) / numberOfSides))
+			);
+		}
+		this.contextReal.closePath();
+		this.contextReal.stroke();
+		this.contextReal.fill();
+
+		this.contextReal.strokeStyle = curStroke;
+		this.contextReal.lineWidth = curThick;
+
+		cPush();
+		// console.log(cPushArray);
+	}
 }
--- javascript/geometricLine.js
+++ javascript/geometricLine.js
@@ -5,6 +5,8 @@ class GeoLines extends PaintFunction {
 		this.contextReal = contextReal;
 		this.contextReal.lineCap = "round";
 		this.contextReal.lineWidth = 1;
+		this.contextDraft.strokeStyle = curStroke;
+		this.contextReal.strokeStyle = curStroke;
 	}
 
 	onMouseDown(coord, event) {
@@ -21,7 +23,7 @@ class GeoLines extends PaintFunction {
 		this.contextDraft.moveTo(points[0].x, points[0].y);
 		for (var i = 1; i < points.length; i++) {
 			this.contextDraft.lineTo(points[i].x, points[i].y);
-			var nearPoint = points[i - 15];
+			var nearPoint = points[i - 10];
 			if (nearPoint) {
 				this.contextDraft.moveTo(nearPoint.x, nearPoint.y);
 				this.contextDraft.lineTo(points[i].x, points[i].y);
